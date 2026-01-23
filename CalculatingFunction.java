5a6d9953ac7be67684fe0c3fa5ec29d47510add5
Rudra627 <rudrasahu9007@gmail.com>
2026-01-23 20:22:25 +0530

    modifieed

--- CalculatingFunction.java
+++ CalculatingFunction.java
@@ -3,15 +3,11 @@ public class CalculatingFunction {
     public static void main(String[] args) {
         Scanner ob=new Scanner(System.in);
         int n=ob.nextInt();
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            if(i%2==0){
-                sum+=i;
-            }
-            else{
-                sum-=i;
-            }
+        if(n%2==0){
+            System.out.println(n/2);
         }
-        System.out.println(sum);
+        else{
+            System.out.println(-((n + 1)/2));
+        }
     }
 }
